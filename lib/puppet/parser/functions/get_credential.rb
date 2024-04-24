dd0ae36585143bcb70f82ddd03d3d485b0689362
Gene Wood <gene_wood@cementhorizon.com>
2024-04-24 11:40:13 -0700

    Update syntax of functions to Puppet 4.x API

--- lib/puppet/parser/functions/get_credential.rb
+++ lib/puppet/parser/functions/get_credential.rb
@@ -1,28 +1,34 @@
-##
-# call-seq:
-#    get_credential(string) -> string
+# @summary
+#   Fetch a specific credstash credential from the credstash facter fact
+#   whether the fact is a Puppet 3.x stringified hash or a Puppet 4.x
+#   hash. After unescaping the credential return it.
 #
-# Fetch a specific credstash credential from the credstash facter fact
-# whether the fact is a Puppet 3.x stringified hash or a Puppet 4.x
-# hash. After unescaping the credential return it.
-#
-#    get_credential('app_password')  #=> s3cret
-#    get_credential('my_roles')  #=> ["arn:aws:iam::012345678901:role/myrole",
-#                                    "arn:aws:iam::123456789012:role/myrole"]
+#      get_credential('app_password')  #=> s3cret
+#      get_credential('my_roles')  #=> ["arn:aws:iam::012345678901:role/myrole",
+#                                      "arn:aws:iam::123456789012:role/myrole"]
+Puppet::Functions.create_function(:'credstash::get_credential') do
+  # @param credstash_key
+  #   The credstash key to lookup a value for
+  #
+  # @return object
+  #   The value for the given credstash key
+  #
+  dispatch :fetch_credstash_value do
+    param 'String', :credstash_key
+  end
 
-module Puppet::Parser::Functions
-  newfunction(:get_credential, :type => :rvalue) do |args|
+  def fetch_credstash_value(credstash_key)
+
     credstash = lookupvar('credstash')
-    if args.empty? or not args[0].is_a? String
-      nil
-    elsif credstash.is_a? Hash and credstash.has_key? args[0]
-      credstash[args[0]]
+    if credstash.is_a? Hash and credstash.has_key? credstash_key
+      credstash[credstash_key]
     elsif credstash.is_a? String
       # Puppet 3.x with stringify_facts = true (default)
-      credentials = function_destringify([credstash])
-      credentials.has_key?(args[0]) ? function_unescape([credentials[args[0]]]) : nil
+      credentials = call_function('destringify', [credstash])
+      credentials.has_key?(credstash_key) ? function_unescape([credentials[credstash_key]]) : nil
     else
       nil
     end
+
   end
 end
